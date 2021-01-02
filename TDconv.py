a4ab55b84908b29c5c4e7d9a901996cf5569d99c
thedarrch <dtcheung@edu.uwaterloo.ca>
2021-01-01 23:02:35 -0500

    Add TDrates module

--- TDconv.py
+++ TDconv.py
@@ -8,19 +8,27 @@ Created on Sat Dec 26 01:31:25 2020
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import os
+
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 def get_rates():
-    drv_path = "C:/Users/theda/Documents/Coding Projects/TDconv/chromedriver.exe"
-    driver = webdriver.Chrome(drv_path)  # full path of browser driver
-    url = 'https://tool.td.com/fxcal/#/fxcalculator'
-    # opening the url
-    driver.get(url)
-    time.sleep(5)
-    
-    a=[]
-    soup = BeautifulSoup(driver.page_source, "lxml")
-    p_list = soup.find_all('p')
-    for p in p_list:
-        if "CAD" in p.get_text() or "USD" in p.get_text():
-            a.append(p.get_text())
-    return a
+	drv_path = os.getcwd() + "chromedriver.exe"
+	print(drv_path + "aersf")
+	driver = webdriver.Chrome(chrome_options=chrome_options)
+	url = 'https://tool.td.com/fxcal/#/fxcalculator'
+	driver.get(url)
+	time.sleep(5)
+	a=[]
+	soup = BeautifulSoup(driver.page_source, "lxml")
+	p_list = soup.find_all('p')
+	for p in p_list:
+		if "CAD" in p.get_text() or "USD" in p.get_text():
+			a.append(p.get_text())
+	driver.quit()
+	return a
--- app.py
+++ app.py
@@ -7,17 +7,22 @@ Created on Sat Dec 26 15:29:18 2020
 
 import flask
 import time
+import os
 
 from jinja2 import Environment
 from jinja2.loaders import FileSystemLoader
 
-# import TDconv
+import TDconv
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+@app.route('/rates')
+def rates():
+    return TDconv.get_rates()
 
 if __name__ == "__main__":
 	app.run()
